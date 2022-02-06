9dc4a7def594ad067d3bd172bcde4bf19d05b1ad
SeungmiNa <naissme0@gmail.com>
2022-02-05 23:45:39 -0800

    feat: make result page

---
+++ frontend/src/pages/Result.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Result.css";
+import styled from 'styled-components';
+
+
+const Result = () => {
+    return(
+        <>
+        </>
+    );
+}
+export default Result;
