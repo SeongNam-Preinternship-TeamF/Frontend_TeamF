d42012b5d3818ab8874699b0a37050282d1ebe78
SeungmiNa <naissme0@gmail.com>
2022-02-05 23:45:11 -0800

    feat: make a nav bar and fix Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route ,Routes} from 'react-router-dom';
 import Main from './pages/Main';
+import Header from './components/Header';
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Main/>}/>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Main/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,27 @@
-import React from 'reeact'
+import React from 'react';
 import styled from 'styled-components';
-import { Route,Routes,Link} from 'react-router-dom';
 
-const Logo=styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 5vh;
-    background-image: url('../../public/images/Logo.png');
+const Logo = styled.div`
+    background-image: url('../public/images/Logo.png');
 `;
 
-const Header = ()=>{
+const Header = () => {
     return(
-        <>
-            <Link to = "../" ><Logo></Logo></Link>
-        </>
-    )
+        <nav style={{width:"100wv"}} class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#"><Logo />F-lower</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">홈 <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">꽃 찾기</a>
+                <a class="nav-item nav-link" href="#">내 꽃은</a>
+                <a class="nav-item nav-link disabled" href="#">캘린더</a>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
-export default Header
+export default Header;
